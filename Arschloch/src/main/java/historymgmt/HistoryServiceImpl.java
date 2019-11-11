85f398a95b4cad4685022dfd580778b0f7dc2e35
Kaya Löher <kayaloeher@Kayas-MacBook-Pro.local>
2019-11-11 20:43:32 +0100

    Frontend und Historymgmt 2

---
+++ Arschloch/src/main/java/historymgmt/HistoryServiceImpl.java
@@ -0,0 +1,26 @@
+package historymgmt;
+
+import gamemgmt.GameInstance;
+import playermgmt.Player;
+
+import java.util.List;
+
+/**
+ * @authors         Kaya Löher 				| Kim Anh Nguyen 		| Christian Wahnsiedler
+ * Email-Adresse: 	s0564784@htw-berlin.de	| s0563958@htw-berlin.de| s0557193@htw-berlin.de
+ */
+public class HistoryServiceImpl implements HistoryService {
+
+    public History saveResultToHistory(GameInstance game){
+        return null;
+    }
+
+    public void persist(GameInstance game){
+
+    }
+
+    public List<Player> getResult(GameInstance game){
+        return null;
+    }
+
+}
