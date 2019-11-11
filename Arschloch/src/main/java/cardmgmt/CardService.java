7f6612a337ae2ee1c31bab4293d130e1419f5231
Kaya Löher <kayaloeher@Kayas-MacBook-Pro.local>
2019-11-11 12:32:25 +0100

    bla

--- Arschloch/src/main/java/cardmgmt/CardService.java
+++ Arschloch/src/main/java/cardmgmt/CardService.java
@@ -6,37 +6,46 @@ import playermgmt.PlayerService;
 
 public interface CardService {
 
-	// getter und setter in domain klassen 
-	
-	/**
-	 * Gibt die Zahl einer Karte zurück.
-	 * 
-	 * @return: Zahl der Karte
-	 */
-	public int getNumber();
-	
-	
-	/**
-	 * Gibt die Farbe einer Karte zurück.
-	 * 
-	 * @return: Farbe der Karte
-	 */
-	public String getSymbol();
-	
-	
-	public List orderCardsByValue(List cards);
+    // getter und setter in domain klassen
+
+    /**
+     * Gibt die Zahl einer Karte zurück.
+     *
+     * @return: Zahl der Karte
+     */
+    public int getNumber();
+
+
+    /**
+     * Gibt die Farbe einer Karte zurück.
+     *
+     * @return: Farbe der Karte
+     */
+    public String getSymbol();
+
+    /**
+     * @param cards
+     * @return
+     */
+    public List orderCardsByValue(List cards);
 
 // methoden die palyer benutzen in palyer verschieben
 
-	/**
-	 * 
-	 * @param player
-	 * @param selectedCards
-	 */
-	public void removeFromHand(PlayerService player, List selectedCards);
-	
-	public void addToHand(PlayerService player, List selectedCards);
-	
-	public List generateDeck();
-	
+    /**
+     * @param player
+     * @param selectedCards
+     */
+    public void removeFromHand(PlayerService player, List selectedCards);
+
+    /**
+     * @param player
+     * @param selectedCards
+     */
+    public void addToHand(PlayerService player, List selectedCards);
+
+    /**
+     * @return
+     */
+    public List generateDeck();
+
 }
