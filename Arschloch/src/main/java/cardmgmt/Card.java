08faaf27f629b571e427d7664a41cbc67e62884c
kmnhngyn <s0563958@htw-berlin.de>
2019-11-11 12:33:56 +0100

    cardmgmt methoden angepasst

--- Arschloch/src/main/java/cardmgmt/Card.java
+++ Arschloch/src/main/java/cardmgmt/Card.java
@@ -12,20 +12,71 @@ package cardmgmt;
  *
  */
 public class Card{
-	int number;
-	String symbol;
+	
+	public enum Zahl {
 
-	/**
-	 * Der Konstruktor bildet eine Karte bestehend aus Zahl und Farbe der Karte.
-	 * 
-	 * @param number: Zahl einer Karte
-	 * @param symbol: Farbe einer Karte
-	 * @return
-	 */
-	Card(int number, String symbol) {
-		this.number = number;
-		this.symbol = symbol;
+		SIEBEN, ACHT, NEUN, ZEHN, BUBE , DAMEN, KOENIG, ASS;
+
 	}
+
+	public enum Symbol {
+
+		KARO, HERZ, PIK, KREUZ;
+
+	}
+
+	final Zahl zahl;
+	final Symbol symbol;
+	
+	public Card (Zahl zahl, Symbol symbol) {
+		this.zahl = zahl;
+		this.symbol = symbol;
+
+	}
+	
+	public Zahl getZahl() {
+		return zahl;
+	}
+	
+	public Symbol getSymbol() {
+		return symbol;
+	}
+	
+	
+	
+//	int number;
+//	String symbol;
+//
+//	/**
+//	 * Der Konstruktor bildet eine Karte bestehend aus Zahl und Farbe der Karte.
+//	 * 
+//	 * @param number: Zahl einer Karte
+//	 * @param symbol: Farbe einer Karte
+//	 * @return
+//	 */
+//	Card(int number, String symbol) {
+//		this.number = number;
+//		this.symbol = symbol;
+//	}
+//	
+//	/**
+//	 * Gibt die Zahl einer Karte zurück.
+//	 * 
+//	 * @return: Zahl der Karte
+//	 */
+//	public int getNumber() {
+//		return number;
+//	};
+//	
+//	
+//	/**
+//	 * Gibt die Farbe einer Karte zurück.
+//	 * 
+//	 * @return: Farbe der Karte
+//	 */
+//	public String getSymbol() {
+//		return symbol;
+//	}
 
 	
 }
