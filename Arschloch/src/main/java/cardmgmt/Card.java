7e24555dcd6dc2def5679085c9553b108f73c6f9
Kaya Löher <kayaloeher@Kayas-MacBook-Pro.local>
2019-11-11 13:13:14 +0100

    Card Management Doc ready

--- Arschloch/src/main/java/cardmgmt/Card.java
+++ Arschloch/src/main/java/cardmgmt/Card.java
@@ -1,82 +1,60 @@
 package cardmgmt;
 
-
 /**
- * 
- *
- * @authors 		Kaya Löher 				| Kim Anh Nguyen 		| Christian Wahnsiedler 
+ * @authors         Kaya Löher 				| Kim Anh Nguyen 		| Christian Wahnsiedler
  * Email-Adresse: 	s0564784@htw-berlin.de	| s0563958@htw-berlin.de| s0557193@htw-berlin.de
- * 
+ *
  * Diese Klasse stellt eine Karte dar.
  * Eine Karte besteht aus einer Zahl und einer Farbe.
- *
  */
-public class Card{
-	
-	public enum Zahl {
+public class Card {
 
-		SIEBEN, ACHT, NEUN, ZEHN, BUBE , DAMEN, KOENIG, ASS;
+    /**
+     * Ein Enum, welches die Zahlen darstellt, welche für ein Kartenspiel benötigt werden.
+     */
+    public enum Zahl {
 
-	}
+        SIEBEN, ACHT, NEUN, ZEHN, BUBE, DAMEN, KOENIG, ASS;
 
-	public enum Symbol {
+    }
 
-		KARO, HERZ, PIK, KREUZ;
+    /**
+     * Ein Enum, welches die Farben darstellt, die für ein Kartenspiel benötigt werden.
+     */
+    public enum Symbol {
 
-	}
+        KARO, HERZ, PIK, KREUZ;
 
-	final Zahl zahl;
-	final Symbol symbol;
-	
-	public Card (Zahl zahl, Symbol symbol) {
-		this.zahl = zahl;
-		this.symbol = symbol;
+    }
 
-	}
-	
-	public Zahl getZahl() {
-		return zahl;
-	}
-	
-	public Symbol getSymbol() {
-		return symbol;
-	}
-	
-	
-	
-//	int number;
-//	String symbol;
-//
-//	/**
-//	 * Der Konstruktor bildet eine Karte bestehend aus Zahl und Farbe der Karte.
-//	 * 
-//	 * @param number: Zahl einer Karte
-//	 * @param symbol: Farbe einer Karte
-//	 * @return
-//	 */
-//	Card(int number, String symbol) {
-//		this.number = number;
-//		this.symbol = symbol;
-//	}
-//	
-//	/**
-//	 * Gibt die Zahl einer Karte zurück.
-//	 * 
-//	 * @return: Zahl der Karte
-//	 */
-//	public int getNumber() {
-//		return number;
-//	};
-//	
-//	
-//	/**
-//	 * Gibt die Farbe einer Karte zurück.
-//	 * 
-//	 * @return: Farbe der Karte
-//	 */
-//	public String getSymbol() {
-//		return symbol;
-//	}
+    final Zahl zahl;
+    final Symbol symbol;
 
-	
+    /**
+     * Eine Karte besteht aus einer Zahl und einer Farbe.
+     * @param zahl: Die Zahl der Karte
+     * @param symbol: Die Farbe der Karte
+     */
+    public Card(Zahl zahl, Symbol symbol) {
+        this.zahl = zahl;
+        this.symbol = symbol;
+
+    }
+
+    /**
+     * Gibt eine Zahl zurück
+     * @return: Eine Zahl
+     */
+    public Zahl getZahl() {
+        return zahl;
+    }
+
+    /**
+     * Gibt eine Farbe zurück
+     * @return: Eine Farbe
+     */
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
 }
