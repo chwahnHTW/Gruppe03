78e7cca0e3f5e2c1ed06167b2a9c7762ea428a72
kmnhngyn <s0563958@htw-berlin.de>
2019-12-25 18:55:37 +0100

    testklassen

--- Arschloch/src/test/java/gamemgmtTest/GameInstanceServiceTest.java
+++ Arschloch/src/test/java/gamemgmtTest/GameInstanceServiceTest.java
@@ -1,32 +1,33 @@
-//package gamemgmtTest;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import kbe.cardmgmt.Card;
-//import kbe.gamemgmt.GameInstance;
-//import kbe.gamemgmt.GameInstanceService;
-//import kbe.gamemgmt.GameInstanceServiceImpl;
-//import kbe.playermgmt.Player;
-//
-///**
-// * @authors         Kaya Löher 				| Kim Anh Nguyen 		| Christian Wahnsiedler
-// * Email-Adresse: 	s0564784@htw-berlin.de	| s0563958@htw-berlin.de| s0557193@htw-berlin.de
-// * <p>
-// * In dieser Klasse werden alle Methoden aus dem GameInstance-Management getestet.
-// */
-//public class GameInstanceServiceTest {
-//
-//    private GameInstanceService service;
-//
-//    @Before
-//    public void setUp() {
-//        service = new GameInstanceServiceImpl();
-//    }
+package gamemgmtTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import kbe.cardmgmt.Card;
+import kbe.gamemgmt.GameInstance;
+import kbe.gamemgmt.GameInstanceService;
+import kbe.gamemgmt.GameInstanceServiceImpl;
+import kbe.playermgmt.Player;
+
+/**
+ * @authors         Kaya Löher 				| Kim Anh Nguyen 		| Christian Wahnsiedler
+ * Email-Adresse: 	s0564784@htw-berlin.de	| s0563958@htw-berlin.de| s0557193@htw-berlin.de
+ * <p>
+ * In dieser Klasse werden alle Methoden aus dem GameInstance-Management getestet.
+ */
+public class GameInstanceServiceTest {
+
+    private GameInstanceService service;
+
+    @Before
+    public void setUp() {
+        service = new GameInstanceServiceImpl();
+    }
 //
 ////    @Test
 ////    public void testDeterminePlayercount() {
@@ -84,16 +85,41 @@
 //        Assert.assertTrue(currentPlayer != nextPlayer);
 //    }
 //
-//    @Test
-//    public void testCalculateGameState() {
-//        GameInstance gi = new GameInstance();
-//        List<Card> testList = new LinkedList<Card>();
-//        Player player = new Player("Tom", 0, testList, null);
-//        Player player2 = new Player("Grit", 1, testList, null);
-//        gi.players.add(player);
-//        gi.players.add(player2);
-//        Assert.assertEquals(service.calculateGameState(gi) == "Running", true);
-//    }
+    @Test
+    public void testCalculateGameStateFinished() {
+        GameInstance gi = new GameInstance();
+        List<Card> handsPlayer1 = new LinkedList<Card>();
+        List<Card> handsPlayer2 = new LinkedList<Card>();
+        Card card1 = new Card(Card.Zahl.DAMEN, Card.Symbol.HERZ);
+        handsPlayer1.add(card1);
+        Player player1 = new Player("Player1", handsPlayer1, null);
+//        player1.setHandCards(handsPlayer1);
+        Player player2 = new Player("Player2", handsPlayer2, null);
+        List<Player> players = new ArrayList<Player> ();
+        players.add(player1);
+        players.add(player2);
+        gi.setPlayers(players);
+        System.out.println(gi.getPlayers().get(0).getHand().isEmpty());
+        Assert.assertEquals(service.calculateGameState(gi) == "Finished", true);
+    }
+    
+    @Test
+    public void testCalculateGameStateRunning() {
+        GameInstance gi = new GameInstance();
+        List<Card> handsPlayer = new LinkedList<Card>();
+        Card card = new Card(Card.Zahl.DAMEN, Card.Symbol.HERZ);
+        handsPlayer.add(card);
+        Player player1 = new Player("Player1", handsPlayer, null);
+        Player player2 = new Player("Player2", handsPlayer, null);
+//        player1.setHandCards(handsPlayer);
+//        player2.setHandCards(handsPlayer);
+        List<Player> players = new ArrayList<Player> ();
+        players.add(player1);
+        players.add(player2);
+        gi.setPlayers(players);
+        System.out.println(gi.getPlayers().get(1).getHand());
+        Assert.assertEquals(service.calculateGameState(gi) == "Running", true);
+    }
 //
 //    @Test
 //    public void testCalculateInitialPlayer() {
@@ -111,4 +137,4 @@
 //        Assert.assertEquals(cards, null);
 //    }
 //
-//}
+}
--- Arschloch/src/test/java/playermgmtTest/PlayerServiceTest.java
+++ Arschloch/src/test/java/playermgmtTest/PlayerServiceTest.java
@@ -1,5 +1,6 @@
 package playermgmtTest;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import kbe.cardmgmt.Card;
+import kbe.gamemgmt.GameInstance;
 import kbe.playermgmt.Player;
 import kbe.playermgmt.PlayerService;
 import kbe.playermgmt.PlayerServiceImpl;
@@ -20,7 +22,6 @@ import kbe.playermgmt.PlayerServiceImpl;
  */
 public class PlayerServiceTest {
 
-
     private PlayerService service;
 
     @Before
@@ -28,15 +29,15 @@ public class PlayerServiceTest {
         service = new PlayerServiceImpl();
     }
 
+//    @Test
+//    public void testGenerateId() {
+//        int x = (int) ((Math.random() * ((1000000 - 1) + 1)) + 1);
+//        System.out.println(x);
+//        Assert.assertEquals(x > 0, true);
+//    }
+    
     @Test
-    public void testGenerateId() {
-        int x = (int) ((Math.random() * ((1000000 - 1) + 1)) + 1);
-        System.out.println(x);
-        Assert.assertEquals(x > 0, true);
-    }
-
-    @Test
-    public void testHasCards() {
+    public void testHasCardsFalse() {
         Player player = new Player("Kaya", null, null);
         Assert.assertEquals(service.hasCards(player), false);
     }
@@ -48,35 +49,138 @@ public class PlayerServiceTest {
         player.setHand(card1);
         Assert.assertEquals(service.hasCards(player), true);
     }
-
+    
     @Test
     public void testRemoveFromHand() {
-        Player player = new Player("Kaya", null, null);
-        Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KARO);
-        Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.KARO);
-        List cardsToBeRemoved = new LinkedList<Card>();
-        cardsToBeRemoved.add(card1);
-
-        player.setHand(card1);
-        player.setHand(card2);
-
-        service.removeFromHand(player, cardsToBeRemoved);
-
-        Assert.assertTrue(player.getHand().size() == 1);
+    	Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KARO);
+    	Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.HERZ);
+    	List<Card> handCards = new ArrayList<Card>();
+    	handCards.add(card1);
+    	handCards.add(card2);
+    	System.out.println(handCards);
+    	Player player = new Player("TestPlayer", handCards, null);
+    	
+    	service.removeFromHand(player, handCards);
+    	System.out.println(player.getHand());
+    	Assert.assertTrue(player.getHand().isEmpty());
     }
 
     @Test
     public void testAddToHand() {
-        Player player = new Player("Kaya", null, null);
+    	Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KARO);
+    	Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.HERZ);
+    	List<Card> handCards = new ArrayList<Card>();
+    	handCards.add(card1);
+    	handCards.add(card2);
+        Player player = new Player("Kaya", handCards, null);
 
-        Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.KARO);
-        List cards = new LinkedList<Card>();
-        cards.add(card2);
+        Card card3 = new Card(Card.Zahl.NEUN, Card.Symbol.PIK);
+        Card card4 = new Card(Card.Zahl.ZEHN, Card.Symbol.KREUZ);
+        List<Card> cardsToAdd = new ArrayList<Card>();
+        cardsToAdd.add(card3);
+        cardsToAdd.add(card4);
+//        System.out.println(cardsToAdd);
+//        System.out.println(handCards);
+        
+        service.addToHand(player, cardsToAdd);
+//        System.out.println(player.getHand());
+//        Assert.assertTrue(player.getHand().size() == cards.size() + player.getHand().size() - cards.size());
 
-        service.addToHand(player, cards);
-
-        Assert.assertTrue(player.getHand().size() == cards.size() + player.getHand().size() - cards.size());
+        Assert.assertEquals(cardsToAdd.get(0), player.handCards.get(0));
+        Assert.assertEquals(cardsToAdd.get(1), player.handCards.get(1));
+    }
+    
+    @Test
+    public void testGetNextPlayer() {
+    	System.out.println("**************** TEST GETNEXTPLAYER OHNE KARO7****************");
+    	GameInstance gi = new GameInstance();
+    	Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.HERZ);
+    	Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.PIK);
+    	Card card3 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KREUZ);
+    	List<Card> handCardsEins = new ArrayList<Card>();
+    	List<Card> handCardsZwei = new ArrayList<Card>();
+    	List<Card> handCardsDrei = new ArrayList<Card>();
+    	handCardsEins.add(card1);
+//    	handCardsZwei.add(card2);
+    	handCardsDrei.add(card3);
+    	
+    	Player player1 = new Player("Player Eins", null, null);
+//    	player1.setHandCards(handCardsEins);
+    	Player player2 = new Player("Player Zwei", null, null);
+//    	player2.setHandCards(handCardsZwei);
+    	Player player3 = new Player("Player Drei", null, null);
+//    	player3.setHandCards(handCardsDrei);
+    	List<Player> players = new ArrayList<Player>();
+    	players.add(player1);
+    	players.add(player2);
+    	players.add(player3);
+    	gi.setPlayers(players);
+    	
+    	gi.setCurrentPlayer(player1);
+//    	gi.setCurrentPlayer(service.getNextPlayer(gi));
+    	Assert.assertEquals(service.getNextPlayer(gi), player3);
+    }
+    
+    @Test
+    public void testSetFirstPlayer() {
+    	System.out.println("**************** TEST SETFIRSTPLAYER MIT KARO7 ****************");
+    	GameInstance gi = new GameInstance();
+    	
+    	Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.HERZ);
+    	Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.PIK);
+    	Card card3 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KARO);
+    	List<Card> handCardsEins = new ArrayList<Card>();
+    	List<Card> handCardsZwei = new ArrayList<Card>();
+    	List<Card> handCardsDrei = new ArrayList<Card>();
+    	handCardsEins.add(card1);
+    	handCardsZwei.add(card2);
+    	handCardsDrei.add(card3);
+    	
+    	Player player1 = new Player("Player Eins", null, null);
+//    	player1.setHandCards(handCardsEins);
+    	Player player2 = new Player("Player Zwei", null, null);
+//    	player2.setHandCards(handCardsZwei);
+    	Player player3 = new Player("Player Drei", null, null);
+//    	player3.setHandCards(handCardsDrei);
+    	List<Player> players = new ArrayList<Player>();
+    	players.add(player1);
+    	players.add(player2);
+    	players.add(player3);
+    	gi.setPlayers(players);
+    	
+    	gi.setCurrentPlayer(service.getNextPlayer(gi));
+    	Assert.assertEquals(gi.getCurrentPlayer(), player3);
     }
 
-
+    @Test
+    public void testSetNewPlayer() {
+    	System.out.println("**************** TEST SETNEWTPLAYER ****************");
+    	GameInstance gi = new GameInstance();
+    	
+    	Card card1 = new Card(Card.Zahl.SIEBEN, Card.Symbol.HERZ);
+    	Card card2 = new Card(Card.Zahl.ACHT, Card.Symbol.PIK);
+    	Card card3 = new Card(Card.Zahl.SIEBEN, Card.Symbol.KREUZ);
+    	List<Card> handCardsEins = new ArrayList<Card>();
+    	List<Card> handCardsZwei = new ArrayList<Card>();
+    	List<Card> handCardsDrei = new ArrayList<Card>();
+    	handCardsEins.add(card1);
+    	handCardsZwei.add(card2);
+    	handCardsDrei.add(card3);
+    	
+    	Player player1 = new Player("Player Eins", null, null);
+//    	player1.setHandCards(handCardsEins);
+    	Player player2 = new Player("Player Zwei", null, null);
+//    	player2.setHandCards(handCardsZwei);
+    	Player player3 = new Player("Player Drei", null, null);
+//    	player3.setHandCards(handCardsDrei);
+    	List<Player> players = new ArrayList<Player>();
+    	players.add(player1);
+    	players.add(player2);
+    	players.add(player3);
+    	gi.setPlayers(players);
+    	
+    	gi.setCurrentPlayer(player1);
+    	gi.setCurrentPlayer(service.getNextPlayer(gi));
+    	Assert.assertEquals(gi.getCurrentPlayer(), player2);
+    }
 }
