47fde72ec481675b0c711455ba1ae1cc185432c1
kmnhngyn <s0563958@htw-berlin.de>
2019-12-25 18:37:59 +0100

    frontendview: getUserCountInput mit try catch

--- Arschloch/src/main/java/kbe/frontendmgmt/FrontendView.java
+++ Arschloch/src/main/java/kbe/frontendmgmt/FrontendView.java
@@ -178,13 +178,19 @@ public class FrontendView extends JFrame {
 	 *                                  perfekt funktioniert
 	 */
 	private int getUserCountInput() throws IllegalArgumentException {
-		String spieleranzahl = JOptionPane.showInputDialog(null,
-				"Bitte Spieleranzahl eingeben (Spieleranzahl muss 3 sein)");
-		if (spieleranzahl.equals("3") | spieleranzahl.equals("4")| spieleranzahl.equals("5")) {
-			return Integer.valueOf(spieleranzahl);
-		} else {
+		String userinput = JOptionPane.showInputDialog(null,
+				"Bitte Spieleranzahl eingeben (Spieleranzahl muss 3 bis 5 sein)");
+		try{
+			if (userinput.equals("3") | userinput.equals("4")| userinput.equals("5")) {
+				int spieleranzahl = Integer.parseInt(userinput);
+				return Integer.valueOf(spieleranzahl);
+			} else {
+				return getUserCountInput();
+			}
+		} catch (NumberFormatException e){
 			return getUserCountInput();
 		}
+			
 	}
 
 	/**
